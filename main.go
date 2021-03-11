4610c6bcf7d468bed65d53d4d44abbed17bd3f80
akshaykrjain <akshayjain.ecb@gmail.com>
2021-03-11 15:30:31 +0100

    Add sample code

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
-// You can edit this code!
-// Click here and start typing.
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func add_values(a int, b int) int {
+
+	var c = a + b
+
+	return c
+}
 
 func main() {
-	fmt.Println("Hello, 世界")
+
+	var a = 4
+
+	var b = 10
+
+	var c = add_values(a, b)
+
+	fmt.Printf("Sum: %d \n", c)
+
+	a = 5
+
+	b = -2
+
+	c = add_values(a, b)
+
+	fmt.Printf("Sum: %d \n", c)
+
 }
